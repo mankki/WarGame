f9fa7455845cdb9383bcfafd6379895c101c9995
mankki <114470120+mankki@users.noreply.github.com>
2024-04-05 14:55:48 +0300

    Delete menu.gd

--- menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_play_red_pressed():
-	get_tree().change_scene_to_file("res://game_red.tscn")
-
-func _on_play_blue_pressed():
-	get_tree().change_scene_to_file("res://game_blue.tscn")
-
