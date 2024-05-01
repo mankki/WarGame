aa982227b404ae066216b86344809590e1b242b5
qaptoR <rocco.ruscitti@outlook.com>
2024-04-30 17:13:16 -0700

    almost done the refactor

--- scripts/game.gd
+++ scripts/game.gd
@@ -121,26 +121,15 @@ var unit_data = {
 }
 
 
-var total_num_of_pieces = 1 # Should be 0, but 1 makes the code work
-const max_blue_soldiers := 1
-const max_blue_tanks := 1
-const max_blue_radars := 1
-const max_blue_missiles := 1
-const max_blue_airplanes := 1
-var total_max_num_of_pieces = unit_data.soldier.max + unit_data.tank.max + unit_data.radar.max + unit_data.missile.max + unit_data.airplane.max
-
+var team = TeamColor.NONE
 var game_state :GameState = GameState.TEAM
-
-var is_placement_in_progress := false
-
 var instances :Dictionary = {}
-var old_piece_positions_ints = []
+var enemies :Dictionary = {}
 
 var preview_type
 var preview_type_str = ""
 
 var moving_range_center: Vector2
-var tile_before_moving
 
 var last_mouse_position = Vector2.ZERO
 var tile_position
@@ -150,9 +139,7 @@ var tilemap: TileMap
 var selected_instance: Node2D = null	# Viittaus valittuun instanssiin, jonka haluat liikuttaa.
 
 
-var team = TeamColor.NONE
 var moving = false
-var old_tile_position
 var piece_instance
 
 @onready var world_node = $World
@@ -197,8 +184,6 @@ func _process (delta) -> void:
     mouse_position = get_global_mouse_position()
     tile_position = tilemap.local_to_map(mouse_position)
 
-    if moving == false:
-        old_tile_position = tile_position
     if tilemap and mouse_position:
         preview_piece()
         preview(preview_type)
@@ -286,8 +271,6 @@ func moving_range_func():
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
 
 
-
-
 func set_moving_to_false():
     moving = false
 #...
@@ -295,115 +278,59 @@ func set_moving_to_false():
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
 
 
-
 # A function to plant enemy pieces
-func update_other_pieces(piece_positions_ints):
-    for piece in piece_positions_ints:
-        if len(old_piece_positions_ints) < len(piece_positions_ints):
-            old_piece_positions_ints.append(0)
-        print(old_piece_positions_ints)
-
-    if not piece_positions_ints: return
-
-    print("Determining the team...")
-    if team == TeamColor.BLUE:
-        print("Trying to update red's pieces on blue's board...")
-        print("old_piece_positions_ints: ", old_piece_positions_ints)
-        print("piece_positions_ints: ", piece_positions_ints)
-        var correction = -moving_range.size.x/2
-        for i in range(0, len(piece_positions_ints) - 1, 2):
-            if old_piece_positions_ints[i] != piece_positions_ints[i] and old_piece_positions_ints[i+1] != piece_positions_ints[i+1]:
-                print("Updating red's pieces on blue's board")
-                if i + 1 < unit_data.soldier.max * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.soldier.scene)
-                elif i + 1 > unit_data.soldier.max * 2 and i < unit_data.soldier.max * 2 + unit_data.tank.max * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.tank.scene)
-                elif i + 1 > unit_data.tank.max * 2 and i < unit_data.soldier.max * 2 + unit_data.tank.max * 2 + unit_data.radar.max * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.radar.scene)
-                elif i + 1 > unit_data.radar.max * 2 and i < unit_data.soldier.max * 2 + unit_data.tank.max * 2 + unit_data.radar.max * 2 + unit_data.missile.max * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.missile.scene)
-                elif i + 1> unit_data.missile.max * 2 and i < unit_data.soldier.max * 2 + unit_data.tank.max * 2 + unit_data.radar.max * 2 + unit_data.missile.max * 2 + unit_data.airplane.max * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.airplane.scene)
-    elif team == TeamColor.RED:
-        print("Trying to update blue's pieces on red's board...")
-        print("old_piece_positions_ints: ", old_piece_positions_ints)
-        print("piece_positions_ints: ", piece_positions_ints)
-        var correction = -moving_range.size.x/2
-        for i in range(0, len(piece_positions_ints) - 1, 2):
-            if old_piece_positions_ints[i] and old_piece_positions_ints[i] != piece_positions_ints[i] and old_piece_positions_ints[i+1] != piece_positions_ints[i+1]:
-                print("Updating blue's pieces on red's board")
-                if i + 1 < max_blue_soldiers * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.soldier.scene.blue)
-                elif i + 1 > max_blue_soldiers * 2 and i < max_blue_soldiers * 2 + max_blue_tanks * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.tank.scene.blue)
-                elif i +1 > max_blue_tanks * 2 and i < max_blue_soldiers * 2 + max_blue_tanks * 2 + max_blue_radars * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.radar.scene.blue)
-                elif i + 1> unit_data.radar.max * 2 and i < max_blue_soldiers * 2 + max_blue_tanks * 2 + max_blue_radars * 2 + max_blue_missiles * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.missile.scene.blue)
-                elif i + 1> max_blue_missiles * 2 and i < max_blue_soldiers * 2 + max_blue_tanks * 2 + max_blue_radars * 2 + max_blue_missiles * 2 + max_blue_airplanes * 2:
-                    plant_enemy(Vector2(piece_positions_ints[i],piece_positions_ints[i+1]), unit_data.airplane.scene.blue)
-    old_piece_positions_ints = piece_positions_ints
+func update_other_pieces(instances_):
+    print(instances_)
+    if instances_.is_empty(): return
+    enemies.clear()
+    var scene_color = TEAM_STRINGS[(int(team)+1) %2]
+    for pos in instances_.keys():    
+        plant_enemy(pos, unit_data[ instances_[pos] ].scene[scene_color])
 #...
 
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
 
 
 # A function to plant the enemy pieces (and remove the piece instance from the initial tile, that is, the tile we're moving from)
-var piece_instances = {}
-func plant_enemy(tile_position: Vector2, scene):
-    if game_state == GameState.PLAYING:
-        for i in range(0, len(piece_positions_ints) - 1, 2):
-            var piece_positions_Vector2_list = []
-            piece_positions_Vector2_list.append(Vector2(old_piece_positions_ints[i], old_piece_positions_ints[i+1]))
-            if tile_position not in piece_positions_Vector2_list:
-                # Remove the piece_instance from the initial tile
-                var key = str(tile_position.x) + "," + str(tile_position.y)
-                if piece_instance and piece_instances.has(key):
-                    var piece_instance_to_remove = piece_instances[key]
-                    remove_child(piece_instance_to_remove)
-                    piece_instance_to_remove.queue_free() # Free th resources
-                    piece_instances.erase(key) # Remove the piece instance from the dictionary
-                # Plant the enemy pieces
-                print("Planting enemy pieces...")
-                var world_position = tilemap.map_to_local(tile_position)
-                piece_instance = scene.instantiate()
-                piece_instance.position = world_position
-                add_child(piece_instance)
-                # Save the piece_instance to a dictionary
-                piece_instances[key] = piece_instance
-    else:
-        # Remove the piece_instance from the initial tile
-        var key = str(tile_position.x) + "," + str(tile_position.y)
-        if piece_instance and piece_instances.has(key):
-            var piece_instance_to_remove = piece_instances[key]
-            remove_child(piece_instance_to_remove)
-            piece_instance_to_remove.queue_free() # Free th resources
-            piece_instances.erase(key) # Remove the piece instance from the dictionary
-        # Plant the enemy pieces
-        print("Planting enemy pieces...")
-        var world_position = tilemap.map_to_local(tile_position)
-        piece_instance = scene.instantiate()
-        piece_instance.position = world_position
-        add_child(piece_instance)
-        # Save the piece_instance to a dictionary
-        piece_instances[key] = piece_instance
+func plant_enemy(tile_pos_: Vector2, scene):
+    enemies[tile_pos_] = scene.instantiate()
+    enemies[tile_pos_].global_position = tilemap.map_to_local(tile_pos_)
+    add_child(enemies[tile_pos_])
 #...
 
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
 
 
-# Funktion to send data
-func send_data(positions :Dictionary):
-    rpc("receive_data", positions)
+func plant(tile_pos :Vector2, scene :PackedScene, num_of_pieces :int) -> int:
+    if tile_pos in instances.keys(): return num_of_pieces
+
+    instances[tile_position] = scene.instantiate()
+    instances[tile_position].set_meta("piece_type", preview_type_str.get_slice('_', 0))
+    instances[tile_position].global_position = tilemap.map_to_local(tile_pos)
+    add_child(instances[tile_position])
+    # $Terminal/TerminalText.text += '\n>>>' + 'Roger that!'
+
+    send_data(instances)
+
+    return num_of_pieces + 1
+#...
+
+    ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
+
+
+func send_data(instances_ :Dictionary):
+    var send_data :Dictionary = {}
+    for key in instances_.keys(): send_data[key] = instances_[key].get_meta('piece_type')
+
+    rpc("receive_data", send_data)
 #...
 
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
         
 
-# Function to receive data
 @rpc("any_peer", "call_remote")
-func receive_data(positions :Dictionary):
-    update_other_pieces(positions)
+func receive_data(recieve_data_ :Dictionary):
+    update_other_pieces(recieve_data_)
 #...
 
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
@@ -421,26 +348,6 @@ func playing_team (team_ :TeamColor) -> void:
 
 func move_mouse():
     selected_instance.global_position = mouse_position
-#...
-
-    ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
-
-
-func plant(tile_pos :Vector2, scene :PackedScene, num_of_pieces :int) -> int:
-    if tile_pos in instances.keys(): return num_of_pieces
-
-    var world_position: Vector2 = tilemap.map_to_local(tile_pos)
-    var piece_instance = scene.instantiate()
-    add_child(piece_instance)
-
-    piece_instance.set_meta("piece_type", preview_type_str.get_slice('_', 0))
-    piece_instance.global_position = world_position
-    instances[tile_position] = piece_instance
-    # $Terminal/TerminalText.text += '\n>>>' + 'Roger that!'
-
-    send_data(instances)
-
-    return num_of_pieces + 1
 #...
 
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
