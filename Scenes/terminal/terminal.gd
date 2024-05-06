b6e8add3ac520381ea55468e928645ac23f55bb8
qaptoR <rocco.ruscitti@outlook.com>
2024-05-06 06:00:11 -0700

    commit before pull before pushing

--- Scenes/terminal/terminal.gd
+++ Scenes/terminal/terminal.gd
@@ -2,10 +2,11 @@
 
 extends VBoxContainer
 
+@onready var terminal :TextEdit = $PanelContainer/MarginContainer/TerminalText
 
 func print_message (message_ :String) -> void:
-    $TerminalText.text += "\n>>> %s" %message_
-    $TerminalText.scroll_vertical += 1_000_000
+    terminal.text += "\n>>> %s" %message_
+    terminal.scroll_vertical += 1_000_000
 #...
 
     ## - --- --- --- --- ,,, ... ''' qFp ''' ... ,,, --- --- --- --- - ##
